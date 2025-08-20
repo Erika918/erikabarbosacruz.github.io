02f392f00ac0abb653563d5001666db016800554
Erika Barbosa <erikaregis42@gmail.com>
2025-08-20 11:12:35 -0300

    Atualizado

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+ const CACHE_NAME = 'curriculo-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
